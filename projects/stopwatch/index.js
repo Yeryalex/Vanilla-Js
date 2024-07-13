21a6b85ffbde6efd58fda3ea1345d7bed159ab1d
Yery <yeryalex03@gmail.com>
2024-07-13 11:32:08 +0200

    pause and reset buttons finish as well as css style

--- projects/stopwatch/index.js
+++ projects/stopwatch/index.js
@@ -1,14 +1,17 @@
 const timer = document.querySelector(".timer");
 const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+const reset = document.querySelector(".reset");
 
 let startTime = 0;
 let elapsedTime = 0;
 let timerInterval;
 
 function startTimer() {
-  startTime = Date.now();
+  startTime = Date.now() - elapsedTime;
   timerInterval = setInterval(updateTimer, 10);
   start.disabled = true;
+  pause.disabled = false;
 }
 
 function updateTimer() {
@@ -26,4 +29,20 @@ function updateTimer() {
     `${milliseconds < 10 ? "0" : ""}${milliseconds}`;
 }
 
+function pauseTimer() {
+  clearInterval(timerInterval);
+  start.disabled = false;
+  pause.disabled = true;
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  timer.innerText = "00:00:00:00";
+  start.disabled = false;
+  pause.disabled = false;
+}
+
 start.addEventListener("click", startTimer);
+pause.addEventListener("click", pauseTimer);
+reset.addEventListener("click", resetTimer);
