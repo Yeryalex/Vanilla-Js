6291878747058fcfac261f64794b10ef64ff9d7b
Yery <yeryalex03@gmail.com>
2024-07-14 23:48:45 +0200

    last modification

--- projects/random-color/index.js
+++ projects/random-color/index.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 const generate = document.querySelector(".generate");
 
 function hexadecimalColors() {
-  let colorHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+  let colorHex = "0123456789abcdef";
 
   let color = [];
   for (let i = 0; i <= 5; i++) {
