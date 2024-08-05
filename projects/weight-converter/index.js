bc5af0e8aeb624c6d7fe14e258faa37b16ebf6fa
Yery <yeryalex03@gmail.com>
2024-08-05 14:13:03 +0200

    showing result

--- projects/weight-converter/index.js
+++ projects/weight-converter/index.js
@@ -1,11 +1,13 @@
 const poundsInput = document.querySelector("#input");
 const calculate = document.querySelector(".calculate");
+const result = document.querySelector(".result");
 
 calculate.addEventListener("click", () => {
   parseInput(poundsInput);
 });
 
 function parseInput(poundsInput) {
-  let value = parseFloat(poundsInput.value);
-  return value;
+  let pounds = parseFloat(poundsInput.value);
+  let Kg = (pounds * 0.453592).toFixed(2);
+  result.innerHTML = `<p>Your weight in Kg is: ${Kg}</p>`;
 }
