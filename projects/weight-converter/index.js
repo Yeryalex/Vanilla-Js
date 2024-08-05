7254171912e178ff425203915f45c932f747862c
Yery <yeryalex03@gmail.com>
2024-08-05 14:44:15 +0200

    manage erros

--- projects/weight-converter/index.js
+++ projects/weight-converter/index.js
@@ -3,11 +3,29 @@ const calculate = document.querySelector(".calculate");
 const result = document.querySelector(".result");
 
 calculate.addEventListener("click", () => {
-  parseInput(poundsInput);
+  showResult();
 });
 
 function parseInput(poundsInput) {
+  if (nonNumeric(poundsInput)) {
+    result.innerHTML = `<p>Wrong value</p>`;
+    return 0;
+  }
   let pounds = parseFloat(poundsInput.value);
   let Kg = (pounds * 0.453592).toFixed(2);
-  result.innerHTML = `<p>Your weight in Kg is: ${Kg}</p>`;
+  return Kg;
+}
+
+function showResult() {
+  let valueKg = parseInput(poundsInput);
+  result.innerHTML = `<p>Your weight in Kg is: ${valueKg}</p>`;
+}
+
+function nonNumeric(poundsInput) {
+  for (let i = 0; i < poundsInput.length; i++) {
+    if (poundsInput.charCodeAt(i) < 48 || poundsInput.charCodeAt(i) > 57) {
+      return true;
+    }
+  }
+  return false;
 }
