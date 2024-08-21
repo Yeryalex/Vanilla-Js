a553ffce6ac40e31d4a0cee9448e58f1d00540f6
Yery <yeryalex03@gmail.com>
2024-08-21 16:51:47 +0200

    style and error handling done

--- projects/weight-converter/index.js
+++ projects/weight-converter/index.js
@@ -6,8 +6,17 @@ calculate.addEventListener("click", () => {
   showResult();
 });
 
+poundsInput.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    event.preventDefault();
+    showResult();
+  }
+});
+
 function parseInput(poundsInput) {
-  if (nonNumeric(poundsInput)) {
+  if (poundsInput === "") {
+    return -1;
+  } else if (nonNumeric(poundsInput)) {
     return null;
   }
   let pounds = parseFloat(poundsInput);
@@ -20,15 +29,13 @@ function showResult() {
   if (valueKg === null) {
     result.innerHTML = `<p>Pounds must be numeric. Please, insert a correct value</p>`;
     return 0;
+  } else if (valueKg == -1) {
+    result.innerHTML = `<p>Please, insert a value</p>`;
+    return 0;
   }
   result.innerHTML = `<p>Your weight in Kg is: ${valueKg}</p>`;
 }
 
-function nonNumeric(poundsInput) {
-  for (let i = 0; i < poundsInput.length; i++) {
-    if (poundsInput.charCodeAt(i) < 48 || poundsInput.charCodeAt(i) > 57) {
-      return true;
-    }
-  }
-  return false;
+function nonNumeric(input) {
+  return isNaN(input) || input.trim() === "";
 }
