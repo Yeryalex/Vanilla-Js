4533fbe41632650353174a6ac9746dc5def6d18a
Yery <yeryalex03@gmail.com>
2024-08-27 20:05:43 +0200

    trailer video done

--- projects/trailer-popup/index.js
+++ projects/trailer-popup/index.js
@@ -1,10 +1,17 @@
 const button = document.querySelector(".button");
 const icon = document.querySelector(".close-icon");
-const trailer = document.querySelector(".trailer/container");
+const trailer = document.querySelector(".trailer-container");
+const video = document.querySelector("video");
 
+button.addEventListener("click", showScreen);
+icon.addEventListener("click", hideScreen);
 
-button.addEventListener("click", )
+function showScreen() {
+  trailer.classList.remove("active");
+}
 
-function showScreen(){
-
+function hideScreen() {
+  trailer.classList.add("active");
+  video.pause();
+  video.currentTime = 0;
 }
