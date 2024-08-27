97a9a1116c2b39f6c0a18453222294c879084d21
Yery <yeryalex03@gmail.com>
2024-08-27 19:58:39 +0200

    trailer

---
+++ projects/trailer-popup/index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".button");
+const icon = document.querySelector(".close-icon");
+const trailer = document.querySelector(".trailer/container");
+
+
+button.addEventListener("click", )
+
+function showScreen(){
+
+}
